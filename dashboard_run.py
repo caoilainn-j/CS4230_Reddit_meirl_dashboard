4ae6e2b8c46569e5c6dca33c8016458719bac6bc
Caoilainn <caoilainnjohnsson@gmail.com>
2026-04-28 15:16:40 -0700

    update overview tab
    - explanations for post nodes, comment nodes, general cleanup

--- dashboard_run.py
+++ dashboard_run.py
@@ -154,21 +154,51 @@ tab1, tab2, tab3, tab4 = st.tabs([
     "Interpretation & Conclusions"
 ])
 
+# page 1
 with tab1:
-    st.header("Overview Here")
+    st.header("Overview")
+    st.markdown("""
+    The main idea of this page is to give you an introduction to the dataset! Our datasets come from researchers on Kaggle,
+    in which they scrolled relateable humor subreddits on Reddit.com, namely r/meirl and r/me_irl, and collected posts and their
+    comments in order to ask interesting questions about their behavior. While relatively niche, these subreddits in particular are
+    interesting to look at because it exists within the "humor" scope, and it can pose some interesting questions
+    when a lot of negativity is seen, and what that can tell us about commenting behaior on Reddit for these communities.
+    You will see how many post nodes and comment nodes (that correspond to a post node) will exist in our network, where we dive deeper
+    into what a post's comment section can look like in regard to "sentiment."
+    
+    *Sentiment* is what the researchers who collected this datset as the overall tone of a comment, with
+    the value of a comment's sentiment existing between 1 (completely positive comment) and -1 (completely negative comment.)
+    While sentiment isn't used usefully in this tab, it becomes more important to look at in the later tabs, as we are able to determine what a 
+    post's comment section overall tone is and how it can impact what kinds of comments occur over time.          
+    """)
     # load in data here
     post_nodes, comment_nodes, dist = load_data()
+    
+    st.markdown("""
+    ## Research Questions
+       Below are the questions that are guiding our research, and what helps us structure our network configurations, graphs, and analysis:
+       #### 1. Do comments from the same post share the same sentiment?
+       #### 2. Regarding sentiment, how does a post's comment section change over time?
+       #### 3. Do posts that already have a lot of comments tend to attract more over time?        
+    """)
 
+    st.markdown("""
+    ## Dataset Details
+    """)
     c1, c2 = st.columns(2)
     c1.metric("Post Nodes", len(post_nodes))
     c2.metric("Comment Nodes", len(comment_nodes))
     st.markdown("""
     ### Comment Distribution Among Posts
-    Explanation here
+    The comment distribution shows the proportion of posts (shown on the left have side) that have
+    a given number of comments (shown on the right side.) This can help give an idea of what the post nodes
+    in the network will look like, with more posts having fewer comments and fewer posts having a lot of comments.
+            
+    This also gives a sneak-peak into what the degree distribution will look like, detailed more in the next tab...
     """)
     dist = dist.to_frame()
-
-    with st.expander("Post Nodes"):
+    # distribution dropdown
+    with st.expander("Comment Distribution Dropdown"):
         st.dataframe(
             dist[[
                 "proportion" 
@@ -176,24 +206,54 @@ with tab1:
         use_container_width=True
         )
 
+    
     st.markdown("""
     ### Post Nodes
-    Description of post nodes
+    The post nodes in our network are representative of the posts found in the r/meirl and r/me_irl
+    subreddits. While cut down after cleaning, we have just under 10,000 posts thta all have comments attached to them, as
+    posts that have no comments within this dataset are not very useful to the questions we are trying to answer.
+                
+    Each post node has four attributes of interest:
+    1. 'id': the unique indentification value of the post
+                
+    2. 'created_utc': the timestamp that indicates when the post was posted in the subreddit it belongs to
+    
+    3. 'permalink': the link to the post on Reddit.com
+                
+    4. 'score': the cumulative number of upvotes/downvotes on a post
+                
+    Following below is a dropdown of all the posts intended for our network, along with their attributes:
     """)
-    with st.expander("Post Nodes"):
+    # post node dropdown
+    with st.expander("Post Nodes Dropdown"):
         st.dataframe(
             post_nodes[[
                 "id", "created_utc", "permalink", "score"
-                
             ]],
         use_container_width=True
         )
 
     st.markdown("""
     ### Comment Nodes
-    Description of post nodes
+    The comment nodes in our network represent the comments on a respective post found in the subreddits. This is a significantly
+    larger dataset, with over 69,000 comments, each being connected to exactly one post. The attributes of interest for the comment nodes
+    are:
+    1. 'id': the unique indentification value of the comment
+    
+    2. 'created_utc': the timestamp that indicates when the comment was posted on the post it belongs to
+                
+    3. 'permalink': the link to the comment on Reddit.com
+                
+    4. 'body': the actual contents of the comment itself / what the comment says
+                
+    5. 'sentiment': the sentiment score (described above) of comment, indicating where on a scale (-1 to 1) the tone of a comment lies
+                
+    6. 'post_id': the identification value of the post the comment is posted on
+
+    Following below is a dropdown of all the comments intended for our network, along with their attributes:          
     """)
-    with st.expander("Comment Nodes"):
+    # comment node dropdown
+    with st.expander("Comment Nodes Dropdown"):
         st.dataframe(
             comment_nodes[[
                 "id", "created_utc", "permalink", "body", "sentiment", "post_id"
@@ -201,11 +261,14 @@ with tab1:
         use_container_width=True
         )
 
+# page 2
 with tab2:
     st.header("Graph Structure, Degree Distribution Here")
 
+# page 3
 with tab3:
     st.header("Sentiment Here")
 
+# page 4
 with tab4:
     st.header("Conclusion Here")
